2b8130882c372d328c9413571f17446eb6f4735a
OpenSourceDavy <davy3232323@gmail.com>
2023-03-01 22:50:13 -0500

    optimized

--- client.sh
+++ client.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#Run script for client distributed as part of 
-#Assignment 1
-#Computer Networks (CS 456)
-#Number of parameters: 5
-#Parameter:
-#    $1: <server_address>
-#    $2: <n_port>
-#    $3: <mode>
-#    $4: <req_code>
-#    $5: <file_received>
+
 
 #Uncomment/update exactly one of the following commands depending on your implementation
 
--- server.sh
+++ server.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#Run script for the server distributed as a part of 
-#Assignment 1
-#Computer Networks (CS 456)
-#Number of parameters: 2
-#Parameter:
-#    $1: <req_code>
-#    $2: <file_to_send>
-
-#Uncomment/update exactly one of the following commands depending on implementation
 
 #For C/C++ implementation
 #./server $1 "$2"
